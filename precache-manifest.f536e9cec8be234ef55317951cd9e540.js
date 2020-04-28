7984cc6cfa8572e2f67b6f4369ace4283c7aed8a
Mudasir Hussain <hmudasir714@gmail.com>
2020-04-28 17:34:45 +0500

    Updates

---
+++ precache-manifest.f536e9cec8be234ef55317951cd9e540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14394df68594afd30ef011c6b0d3460f",
+    "url": "/React-simple-App/index.html"
+  },
+  {
+    "revision": "7e4b7bbfa56e554709b7",
+    "url": "/React-simple-App/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "33c262d80e9a17f2fabd",
+    "url": "/React-simple-App/static/js/2.073da32f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-simple-App/static/js/2.073da32f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4b7bbfa56e554709b7",
+    "url": "/React-simple-App/static/js/main.62452c0e.chunk.js"
+  },
+  {
+    "revision": "10feb8d9a0f3ee3c2e16",
+    "url": "/React-simple-App/static/js/runtime-main.780af465.js"
+  }
+]);
